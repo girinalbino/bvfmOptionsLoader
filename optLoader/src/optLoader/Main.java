7d0593d2aef0de7fdebb873f6a5748b11c64f7d0
jeferson <jeferson@jow>
2016-09-07 09:10:39 -0300

    Criação do head

---
+++ optLoader/src/optLoader/Main.java
@@ -0,0 +1,11 @@
+package optLoader;
+
+public class Main {
+
+	// ver https://spring.io/guides/gs/batch-processing/#initial
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
